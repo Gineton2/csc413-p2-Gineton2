b242b159b81d04bafb7fc746ae6a84725325074e
Gineton Alencar II <Gineton2@users.noreply.github.com>
2024-02-05 20:29:10 -0800

    Implement RunTimeStack peek, push, pop functions

--- interpreter/virtualmachine/RunTimeStack.java
+++ interpreter/virtualmachine/RunTimeStack.java
@@ -5,8 +5,8 @@ import java.util.Stack;
 
 class RunTimeStack {
 
-    private ArrayList<Integer> runTimeStack;
-    private Stack<Integer>     framePointer;
+    private ArrayList<Integer> runTimeStack; // values in program
+    private Stack<Integer>     framePointer; // frame boundaries, should never be empty (less than size of 1)
 
     public RunTimeStack() {
         runTimeStack = new ArrayList<>();
@@ -15,5 +15,46 @@ class RunTimeStack {
         // point of our language, so its frame pointer is 0.
         framePointer.add(0);
     }
-    
+
+    private int lastIndex(){
+        return this.runTimeStack.size()-1;
+    }
+
+    // cannot peek an empty stack, but not a concern for given test programs
+    public int peek(){
+        return this.runTimeStack.get(lastIndex());
+    }
+
+    public int push(int valueToPush){
+        this.runTimeStack.add(valueToPush);
+        return this.peek();
+    }
+
+    public int pop(){
+        return this.runTimeStack.remove(lastIndex());
+    }
+
+    public static void  main(String[] args) {
+        // testing push, pop, peek
+        RunTimeStack rs = new RunTimeStack();
+        rs.push(5);
+        rs.push(4);
+        rs.push(3);
+        rs.push(2);
+        rs.push(1);
+
+        rs.runTimeStack.forEach(System.out::println);
+
+        rs.push(10);
+
+        System.out.println(rs.peek());
+        rs.pop();
+        System.out.println(rs.peek());
+        rs.push(100);
+        System.out.println(rs.peek());
+        rs.pop();
+        System.out.println(rs.peek());
+
+
+    }
 }
