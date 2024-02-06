399d54b7aff616ad25ccb61deee89f957671371e
Gineton Alencar II <Gineton2@users.noreply.github.com>
2024-02-05 22:05:08 -0800

    Implement VirtualMachine executeProgram from given documentation

--- interpreter/virtualmachine/VirtualMachine.java
+++ interpreter/virtualmachine/VirtualMachine.java
@@ -1,9 +1,13 @@
 package interpreter.virtualmachine;
 
+import interpreter.bytecode.ByteCode;
+
 import java.util.Stack;
 
+// middleman bw programs and bytecode/runtimestack
 public class VirtualMachine {
 
+    // do not break encapsulation
     private RunTimeStack   runTimeStack;
     private Stack<Integer> returnAddress;
     private Program        program;
@@ -14,6 +18,17 @@ public class VirtualMachine {
         this.program = program;
     }
 
+    // Obtained from given documentation
     public void executeProgram() {
+        programCounter = 0;
+        runTimeStack = new RunTimeStack();
+        returnAddress = new Stack<Integer>();
+        isRunning = true;
+
+        while (isRunning){
+            ByteCode code = program.getCode(programCounter);
+            code.execute(this);
+            programCounter++;
+        }
     }
 }
