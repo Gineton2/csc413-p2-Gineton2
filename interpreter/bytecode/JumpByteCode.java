d0a83a095c7601458eb09b4c6dc0d5d3946d2254
Gineton Alencar II <Gineton2@users.noreply.github.com>
2026-07-20 20:00:57 -0700

    Implement JumpByteCode Abstract Class

--- interpreter/bytecode/JumpByteCode.java
+++ interpreter/bytecode/JumpByteCode.java
@@ -4,35 +4,36 @@ import interpreter.virtualmachine.VirtualMachine;
 
 import java.util.ArrayList;
 
+/**
+ * Jump ByteCode is an abstraction of Call, Goto, and FalseBranch ByteCodes.
+ * These ByteCodes take one argument, a given label, which they jump to.
+ * Address resolution is performed in Program for these ByteCodes to correctly jump to the expected label.
+ */
 public abstract class JumpByteCode extends ByteCode {
-    protected int labelAddress;
-    protected String label;
+    private int labelAddress;
+    private String label;
+
     /**
-     * Initializes each ByteCode.
-     *
-     * @param args the arguments which make up each ByteCode instruction
+     * Initializes a Jump ByteCode such as Call, Goto, or FalseBranch.
+     * JumpByteCodes take one argument containing the label to be jumped to.
+     * @param args a single-element list containing a String of the label to be jumped to.
      */
     @Override
     public void init(ArrayList<String> args) {
+        if (args.size() != 1) {
+            throw new IllegalArgumentException(
+                    "Jump ByteCodes require one argument: a label denoting a location to jump to.");
+        }
+        this.label = args.get(0);
     }
+
+    public String getLabel(){ return this.label; }
 
     /**
-     * Executes each ByteCode according to their specified behavior.
-     *
-     * @param vm the virtual machine that will execute the ByteCodes
+     * Address resolution is performed by Program.resolveAddress() and set here.
+     * @param labelAddress the resolved address of this Jump-type ByteCode
      */
-    @Override
-    public void execute(VirtualMachine vm) {
+    public void setLabelAddress(int labelAddress) { this.labelAddress = labelAddress; }
 
-    }
-
-    /**
-     * Returns a String of the Bytecode and its arguments.
-     *
-     * @return String containing the ByteCode and any arguments.
-     */
-    @Override
-    public String toString() {
-        return null;
-    }
+    public int getLabelAddress(){ return this.labelAddress; }
 }
