af6058ab36438f486d5e6d9cf6ca706825235631
Gineton Alencar II <Gineton2@users.noreply.github.com>
2026-08-05 18:51:50 -0700

    Implement Program.resolveAddress()

--- interpreter/virtualmachine/Program.java
+++ interpreter/virtualmachine/Program.java
@@ -2,46 +2,53 @@ package interpreter.virtualmachine;
 
 import interpreter.bytecode.ByteCode;
 import interpreter.bytecode.JumpByteCode;
+import interpreter.bytecode.LabelCode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The Program class stores ByteCodes and resolves symbolic addresses to absolute addresses.
+ */
 public class Program {
 
-    private ArrayList<ByteCode> program; //must stay private
+    private ArrayList<ByteCode> program; // Must stay private
 
     public Program() {
         program = new ArrayList<>();
     }
-
 
     protected ByteCode getCode(int programCounter) {
         return this.program.get(programCounter);
     }
 
     /**
-     * This function should go through the program and resolve all addresses.
-     * Currently, all labels look like LABEL <<num>>>, these need to be converted into
-     * correct addresses so the VirtualMachine knows what to set the Program Counter
-     * HINT: make note what type of data-structure ByteCodes are stored in.
+     * Maps labels from the compiler to absolute addresses in the program.
+     * Initially, all labels look like `LABEL <<num>>`, these need to be converted into
+     * correct addresses so the VirtualMachine knows what to set the Program Counter.
+     * It iterates through the program to store labels with their indices, then assigns
+     * JumpByteCode types to appropriate addresses.
      */
     public void resolveAddress() {
-        // simple solution would be run for loop twice over program, there are O(n) solution(s) however
         Map<String,Integer> labelAddressesMap = new HashMap<>();
-        // 1st pass through arraylist, keeping track of label codes & their labels
-        for (ByteCode currentByteCode : program) {
-            if (currentByteCode instanceof JumpByteCode) {
+
+        for (int i = 0; i < program.size(); i++) {
+            if (program.get(i) instanceof LabelCode) {
+                LabelCode currentLabelCode = (LabelCode) program.get(i);
+                labelAddressesMap.put(currentLabelCode.getLabel(), i);
             }
         }
-        // 2nd pass through arrayList, look for call, goto, and falsebranch codes
-        // then, look at stored label codes and find matching label value
-        // ex " val || (address)index " >> " (str)continue<<6>> || (int)(line)10 "
-
+        for (ByteCode byteCode : program) {
+            if (byteCode instanceof JumpByteCode) {
+                JumpByteCode jumpCode = (JumpByteCode) byteCode;
+                int jumpCodeAddress = labelAddressesMap.get(jumpCode.getLabel());
+                jumpCode.setLabelAddress(jumpCodeAddress);
+            }
+        }
     }
 
     /**
-     * Program is where ByteCodes are prepared
      * @param bc ByteCode to be added to Program.
      */
     public void addCode(ByteCode bc) { program.add(bc); }
